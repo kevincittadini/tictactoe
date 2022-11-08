dbd31889181ef60e68d82e9568a7412c3e498f72
Kevin Cittadini <cittadini.kevin@gmail.com>
2022-11-08 19:11:41 +0100

    create game API with unit test.

--- application/src/Application/Controller/GameController.php
+++ application/src/Application/Controller/GameController.php
@@ -4,21 +4,33 @@ declare(strict_types=1);
 
 namespace TicTacToe\Application\Controller;
 
+use Ramsey\Uuid\Uuid;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use TicTacToe\Application\Repository\Write\GameRepository;
+use TicTacToe\Domain\Game;
+use TicTacToe\Domain\Id;
 
 final class GameController
 {
     public function __construct(
-        GameRepository $writeGameRepository,
+        private GameRepository $writeGameRepository,
     ) {
     }
 
     #[Route(path: '/api/game', methods: ['POST'])]
     public function createAction(): Response
     {
-        return new JsonResponse();
+        $id = new Id(Uuid::uuid4()->toString());
+
+        $game = Game::default($id);
+
+        $this->writeGameRepository->store($game);
+
+        return new JsonResponse([
+            'gameId' => $id->toString(),
+            'board' => $game->board->status,
+        ]);
     }
 }
