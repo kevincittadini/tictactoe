790cdd65e6d9b82bf299dfc9fc95d206f738ac32
Kevin Cittadini <cittadini.kevin@gmail.com>
2022-11-08 18:25:00 +0100

    configure GameController.

--- application/src/Application/Controller/GameController.php
+++ application/src/Application/Controller/GameController.php
@@ -4,7 +4,22 @@ declare(strict_types=1);
 
 namespace TicTacToe\Application\Controller;
 
-class GameController
-{
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use TicTacToe\Application\Repository\Write\GameRepository;
 
+final class GameController
+{
+    public function __construct(
+        GameRepository $writeGameRepository,
+    ) {
+    }
+
+    #[Route(path: '/api/game', methods: ['POST'])]
+    public function createAction(Request $request): Response
+    {
+        return new JsonResponse();
+    }
 }
