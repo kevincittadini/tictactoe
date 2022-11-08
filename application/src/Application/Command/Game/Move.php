26c897bf063e2f328f9ba457352a36e119e04221
Kevin Cittadini <cittadini.kevin@gmail.com>
2022-11-08 20:47:26 +0100

    create BoardCell type.

---
+++ application/src/Application/Command/Game/Move.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TicTacToe\Application\Command\Game;
+
+use TicTacToe\Application\Command\Command;
+
+final class Move implements Command
+{
+    public function __construct(
+    ) {
+    }
+}
