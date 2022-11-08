2cf451871dedb36eef03db8fa8f04ef802574e08
Kevin Cittadini <cittadini.kevin@gmail.com>
2022-11-08 16:43:48 +0100

    test Board VO in order to keep the board status in place.

--- application/src/Domain/Board.php
+++ application/src/Domain/Board.php
@@ -4,7 +4,24 @@ declare(strict_types=1);
 
 namespace TicTacToe\Domain;
 
-class Board
+final class Board
 {
+    private function __construct(
+        public string $status,
+    ) {
+    }
 
+    public static function fromStatus(string $status): self
+    {
+        if (!self::isValid($status)) {
+            throw new \DomainException("Invalid board status.");
+        }
+
+        return new self($status);
+    }
+
+    private static function isValid(string $status): bool
+    {
+        return preg_match("/[0-2]{9}/", $status) === 1;
+    }
 }
