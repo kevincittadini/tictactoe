40b07a995602477d9da451a2cf48d2b845e8459a
Kevin Cittadini <cittadini.kevin@gmail.com>
2022-11-08 20:49:17 +0100

    add modifiers.

--- application/src/Application/Command/Game/Move.php
+++ application/src/Application/Command/Game/Move.php
@@ -5,10 +5,14 @@ declare(strict_types=1);
 namespace TicTacToe\Application\Command\Game;
 
 use TicTacToe\Application\Command\Command;
+use TicTacToe\Domain\BoardCell;
+use TicTacToe\Domain\Player;
 
 final class Move implements Command
 {
     public function __construct(
+        public readonly Player $player,
+        public readonly BoardCell $boardCell,
     ) {
     }
 }
