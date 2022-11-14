06ce77b79d8328c53ad9cd233183d7fc17e7eb5d
Kevin Cittadini <cittadini.kevin@gmail.com>
2022-11-14 14:36:45 +0100

    implement get game.

--- application/src/Application/Controller/GameController.php
+++ application/src/Application/Controller/GameController.php
@@ -10,9 +10,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use TicTacToe\Application\Command\Game\CreateGame;
 use TicTacToe\Application\Command\Game\Move;
+use TicTacToe\Application\Repository\Read\GameRepository;
 use TicTacToe\Application\Service\CommandHandlerManagerInterface;
+use TicTacToe\Domain\Board;
 use TicTacToe\Domain\BoardCell;
 use TicTacToe\Domain\Game;
+use TicTacToe\Domain\GameStatus;
 use TicTacToe\Domain\Id;
 use TicTacToe\Domain\Player;
 
@@ -20,6 +23,7 @@ final class GameController
 {
     public function __construct(
         private readonly CommandHandlerManagerInterface $commandHandlerManager,
+        private readonly GameRepository $gameRepository,
     ) {
     }
 
@@ -44,6 +48,29 @@ final class GameController
             );
         } catch (\DomainException $e) {
             return new JsonResponse(['error' => $e->getMessage()], Response::HTTP_BAD_REQUEST);
+        } catch (\Throwable $e) {
+            return new JsonResponse(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    #[Route(methods: ["GET"], path: "/api/game/{gameId}")]
+    public function getAction(string $gameId): Response
+    {
+        try {
+            $game = $this->gameRepository->get(new Id($gameId));
+
+            if (null === $game) {
+                throw new \DomainException('Game not found', Response::HTTP_NOT_FOUND);
+            }
+
+            return new JsonResponse([
+                'board' => $game->board->status,
+                'winner' => $game->winner->value,
+                'staleCondition' => $game->board->isInStaleCondition(),
+                'gameStatus' => $game->status->value,
+            ], Response::HTTP_OK);
+        } catch (\DomainException $e) {
+            return new JsonResponse(['error' => $e->getMessage()], $e->getCode());
         } catch (\Throwable $e) {
             return new JsonResponse(['error' => $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
--- application/tests/Unit/Application/GameControllerTest.php
+++ application/tests/Unit/Application/GameControllerTest.php
@@ -12,9 +12,13 @@ use Ramsey\Uuid\Uuid;
 use Symfony\Component\HttpFoundation\Response;
 use TicTacToe\Application\Command\Game\CreateGame;
 use TicTacToe\Application\Controller\GameController;
+use TicTacToe\Application\Repository\Read\GameRepository;
 use TicTacToe\Application\Service\CommandHandlerManagerInterface;
 use TicTacToe\Domain\Board;
 use TicTacToe\Domain\Game;
+use TicTacToe\Domain\GameStatus;
+use TicTacToe\Domain\Id;
+use TicTacToe\Domain\Player;
 
 
 class GameControllerTest extends TestCase
@@ -23,15 +27,18 @@ class GameControllerTest extends TestCase
 
     private ObjectProphecy $commandHandlerManager;
     private GameController $controller;
+    private ObjectProphecy $gameRepository;
 
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->commandHandlerManager = $this->prophesize(CommandHandlerManagerInterface::class);
+        $this->gameRepository = $this->prophesize(GameRepository::class);
 
         $this->controller = new GameController(
-            $this->commandHandlerManager->reveal()
+            $this->commandHandlerManager->reveal(),
+            $this->gameRepository->reveal(),
         );
     }
 
@@ -42,6 +49,44 @@ class GameControllerTest extends TestCase
 
         $this->assertTrue(Uuid::isValid($response['gameId']));
         $this->assertEquals(Board::default()->status, $response['board']);
+    }
+
+    public function test_returns_game_data_structure(): void
+    {
+        $response = $this->responseToArray($this->controller->createAction());
+        $gameId = $response['gameId'];
+
+        $gameDataStructure = [
+            'id' => $gameId,
+            'board' => Board::default()->status,
+            'winner' => Player::NONE->value,
+            'nextPlayer' => Player::TWO->value,
+            'status' => GameStatus::OPEN->value,
+        ];
+
+        $game = Game::fromArray($gameDataStructure);
+
+        $this->gameRepository->get(new Id($gameId))->shouldBeCalled()->willReturn($game);
+
+        $actualResponse = $this->responseToArray($this->controller->getAction($gameId));
+        $expectedResponse = [
+            'board' => Board::default()->status,
+            'winner' => Player::NONE->value,
+            'staleCondition' => false,
+            'gameStatus' => GameStatus::OPEN->value,
+        ];
+
+        $this->assertEquals($expectedResponse, $actualResponse);
+    }
+
+    public function test_it_throws_an_error_if_game_is_not_found(): void
+    {
+        $gameId = '123';
+
+        $this->gameRepository->get(new Id($gameId))->shouldBeCalled()->willReturn(null);
+        $actualResponse = $this->responseToArray($this->controller->getAction($gameId));
+
+        $this->assertEquals(['error' => 'Game not found'], $actualResponse);
     }
 
     private function responseToArray(Response $response): array
